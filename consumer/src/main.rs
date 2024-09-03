5be57cb2dd2e805d6fded3fea1fe743780e8664e
Ismael Pamplona <ismaelpamplona@hotmail.com>
2024-09-03 18:38:05 -0300

    (up) Issue #implement_consumer_handler.

--- consumer/src/main.rs
+++ consumer/src/main.rs
@@ -13,9 +13,11 @@ async fn main() {
     // Set up a connection to RabbitMQ
     let addr = std::env::var("RABBITMQ_ADDR")
         .unwrap_or_else(|_| "amqp://guest:guest@rabbitmq:5672".to_string());
+
     let connection = Connection::connect(&addr, ConnectionProperties::default())
         .await
         .unwrap();
+
     info!("Connected to RabbitMQ");
 
     // Start the consumer

6da2bbe5e235c445a2744be850323db32cb7332b
Ismael Pamplona <ismaelpamplona@hotmail.com>
2024-09-08 08:44:32 -0300

    (up) Issue #write_send_requests_script.

---
+++ send_requests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+URL="http://localhost:3000/request"
+PAYLOAD_TEMPLATE='{"method": "GET", "endpoint": "http://api-simulation:8080/", "payload": null}'
+
+# Loop to send up to 10 requests (rate limit for api-simulation)
+for i in {1..12}
+do
+  curl -X POST $URL \
+  -H "Content-Type: application/json" \
+  -H "X-Request-ID: client-1" \  # Constant request ID
+  -d "$PAYLOAD_TEMPLATE" > /dev/null 2>&1
+done
